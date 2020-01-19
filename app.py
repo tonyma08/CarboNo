ce87ac8580fd2f487f097a6989120df7156848d5
Victor H <60029594+victorh12@users.noreply.github.com>
2020-01-18 17:18:44 -0800

    Remove render function for home and use index instead

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-def render_home():
+def render_index():
     return render_template('index.html')
-
 
 @app.route('/about')
 def render_about():
     return render_template('about.html')
 
-@app.route('/view')
+@app.route('/viewdata')
 def render_view():
     return render_template('viewdata.html')
 
