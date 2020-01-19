96c210aad6b2e70b8f477231bb0c6695ca21147f
Victor H <60029594+victorh12@users.noreply.github.com>
2020-01-18 23:16:19 -0800

    Make login closer to working

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,31 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
-from app.forms import LoginForm
+# Third-party libraries
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+import requests
+from oauthlib.oauth2 import WebApplicationClient
+import os
 
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+    
 @app.route('/')
 def render_index():
     return render_template('index.html', is_authentiated=current_user.is_authenticated)
@@ -17,15 +41,21 @@ def render_view():
 @app.route('/submit')
 def submit_view():
     return render_template('submit.html')
-
-@app.route('/login', methods=['GET', 'POST'])
+   
+@app.route("/login")
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
     
 if __name__ == '__main__':
     app.run(debug=True)
