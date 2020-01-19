8d16e322c5cba1df9fea66f56ddd0849a966f3e1
Victor H <60029594+victorh12@users.noreply.github.com>
2020-01-19 01:53:43 -0800

    Finish electricity daily tracking

--- app/static/js/submit.js
+++ app/static/js/submit.js
@@ -11,19 +11,13 @@ var storeJSON = function(name, data) {
     localStorage.setItem(name, JSON.stringify(data));
 };
 
-/**
- * @param {int} The month number, 0 based
- * @param {int} The year, not zero based, required to account for leap years
- * @return {Date[]} List with date objects for each day of the month
- */
 function getDaysInMonth(month, year) {
-  var date = new Date(year, month, 1);
-  var days = [];
-  while (date.getMonth() === month) {
-    days.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
-  return days;
+    if(month == 2) {
+        return year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0) ? 29 : 28;
+    }
+    else {
+        return (new Array(31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))[month - 1];
+    }
 }
 
 $(document).ready(function() {
@@ -57,6 +51,36 @@ $(document).ready(function() {
             emissionDates[$("input[name='travel-date']").val()] = [emissionID];
         }
         storeJSON('emission-history', emissionHistory);
-        storeJSOn('emission-dates', emissionDates);
+        storeJSON('emission-dates', emissionDates);
+    });
+    
+    $("input[name='electricity-submit']").click(function() {
+        var emissionHistory = getStored('emission-history');
+        var emissionDates = getStored('emission-dates');
+        var emissionID = Object.keys(emissionHistory).length;
+        
+        var month = parseInt($("option[name='electricity-month']:selected").attr("value"), 10);
+        var year = $("input[name='electricity-year']").val();
+        var kWhUse = $("input[name='electricity-amount']").val();
+        var totalDays = getDaysInMonth(month, year);
+        var zeroPadMonth = month.toString().length == 1 ? "0" + month : month;            
+        for(var day = 1; day <= totalDays; day++) {
+            var zeroPadDay = day.toString().length == 1 ? "0" + day : day;
+            var incDate = year + "-" + zeroPadMonth + "-" + zeroPadDay;
+            emissionHistory[emissionID] = {
+                "date": incDate,
+                "type": "electricity",
+                "electricity": { "amount": kWhUse / totalDays }
+            };
+            if(emissionDates[incDate] !== undefined) {
+                emissionDates[incDate].push(emissionID);
+            }
+            else {
+                emissionDates[incDate] = [emissionID];
+            }
+            emissionID++;
+        }
+        storeJSON('emission-history', emissionHistory);
+        storeJSON('emission-dates', emissionDates);
     });
 });
