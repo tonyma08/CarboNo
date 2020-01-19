3744700d60a5a92981abf19835d7cc5a482626a8
ryan-kang01 <ryskang@ucsc.edu>
2020-01-18 16:36:06 -0800

    rewrote function names

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route("/home")
-def main():
-    return render_template('index.html');
+@app.route('/home')
+def render_home():
+    return render_template('index.html')
 
-@app.route('ahout')
-def about():
-    return render_template('about.html');
+"""
+@app.route('/about')
+def render_about(about):
+    return render_template(about.html)
+"""
 
-if__name__ ==" __main__":
-    app.run();
+if __name__ == '__main__':
+    app.run(debug=True)
