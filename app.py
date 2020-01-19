d8c80d782bf917768c60cbe00c4bbae243f42aaf
Stephen Zhu <yzhu156@ucsc.edu>
2020-01-18 16:19:09 -0800

    about.html included

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route("/home")
 def main():
-    return None
+    return render_template('index.html');
 
+@app.route('ahout')
 def about():
-    return None
+    return render_template('about.html');
 
+if__name__ ==" __main__":
+    app.run();
